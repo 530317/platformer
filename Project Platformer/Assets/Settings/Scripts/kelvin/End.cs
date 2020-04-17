3affd0a0061a9b9a9dbc4313f9f3234af4d689c0
530317 <530317@student.glu.nl>
2020-04-17 14:59:58 +0200

    end scene

---
+++ Project Platformer/Assets/Settings/Scripts/kelvin/End.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+    public GameObject shipPart1;
+    public GameObject shipPart2;
+    public GameObject shipPart3;
+    public GameObject shipPart4;
+    public GameObject canvaseind;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        StartCoroutine(ItemPop());
+    }
+    private IEnumerator ItemPop()
+    { 
+        shipPart1.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        shipPart2.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        shipPart3.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        shipPart4.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        canvaseind.SetActive(true);
+    }
+
+}
---
+++ Project Platformer/Assets/Settings/Scripts/kelvin/MoveShip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveShip : MonoBehaviour
+{
+    public Vector3 startPos;
+    public Vector3 endPos;
+
+    public float speed;
+
+    private float step;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, endPos, step);
+        if (transform.position == endPos)
+        {
+            SceneManager.LoadScene("Startmeneu");
+        }
+    }
+}
